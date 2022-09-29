a23ac3b9e8170cfa8ef82bb8dc74b664e302b73a
Alexis Lucattini <alexis.lucattini@umccr.org>
2022-09-29 22:37:48 +1000

    Added ica-make-md5sum script

--- internal-functions/gds_aws_helpers.sh
+++ internal-functions/gds_aws_helpers.sh
@@ -1,8 +1,56 @@
 #!/usr/bin/env bash
 
 : '
-
+AWS helpers
 '
+
+get_aws_write_file_access(){
+  : '
+  Get AWS Access tokens for a file
+  '
+  local volume_name="$1"
+  local gds_file_path="$2"
+  local base_url="$3"
+  local access_token="$4"
+
+  # Other local vars
+  local file_name
+
+  # Set file name
+  file_name="$(basename "${gds_file_path}")"
+  folder_path="$(dirname "${gds_file_path}")"
+
+  # Create a json object as the --data-raw attribute
+  body_arg="$( \
+    jq --null-input --raw-output \
+      --arg "name" "${file_name}" \
+      --arg "volume_name" "${volume_name}" \
+      --arg "folder_path" "${folder_path}/" \
+      --arg "type" "application/json" \
+      '
+        {
+          "name": $name,
+          "volumeName": $volume_name,
+          "folderPath": $folder_path
+        }
+      ' \
+  )"
+
+  # Pipe curl output into jq to collect ID and return
+  file_object="$(curl \
+    --silent \
+    --request POST \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer ${access_token}" \
+    --data-raw "${body_arg}" \
+    "${base_url}/v1/files?include=objectStoreAccess")"
+
+  # Return access credentials
+  jq \
+    --raw-output \
+    '.objectStoreAccess.awsS3TemporaryUploadCredentials' <<< "${file_object}"
+}
+
 
 get_aws_access_creds_from_folder_id() {
   : '
@@ -95,3 +143,5 @@ get_key_prefix_from_credentials() {
   echo "${aws_credentials}" | jq --raw-output '.keyPrefix'
 
 }
+
+
